93b833b1850986862c5eea953d754cb3b801d084
Khalid Waleed <khalidwaleed0@outlook.com>
2024-11-18 20:02:46 +0200

    Added infinite scroll pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.jpeg";
 import "./App.css";
 import Select from "react-select";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { categories, governorates } from "./constants.ts";
 
 function App() {
@@ -9,41 +9,72 @@ function App() {
     { value: "1", label: "Price: low to high" },
     { value: "-1", label: "Price: high to low" },
   ];
-  const [selectedGovernorate, setSelectedGovernorate] = useState(undefined);
-  const [selectedCategory, setSelectedCategory] = useState(undefined);
+  const [selectedGovernorate, setSelectedGovernorate] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
   const [selectedSort, setSelectedSort] = useState(sortOptions[0]);
   const [minPrice, setMinPrice] = useState(NaN);
   const [maxPrice, setMaxPrice] = useState(NaN);
   const [products, setProducts] = useState([]);
+  const isLastPage = useRef(false);
+  const searchValue = useRef("");
+  const page = useRef(1);
 
-  function search(e) {
+  function handleSearchInput(e) {
+    searchValue.current = e.target.value;
     if (e.key === "Enter" && e.target.value !== "") {
-      fetch(
-        "http://127.0.0.1:3000/search?" +
-          new URLSearchParams({
-            name: e.target.value,
-            category: selectedCategory?.map((cat) => cat.value)?.join(","),
-            location: selectedGovernorate?.map((gov) => gov.value)?.join(","),
-            minPrice: minPrice || 0,
-            maxPrice: maxPrice || 9000000,
-            sortAsc: 1,
-          }),{
-            headers:{
-              cacheControl: "noCache",
-            }
-          }
-      )
-        .then((res) => res.json())
-        .then((res) => setProducts(res));
+      isLastPage.current = false;
+      page.current = 1;
+      setProducts([]);
+      fetchProducts();
+    } else searchValue.current = e.target.value;
+  }
+  function fetchProducts() {
+    if (searchValue.current === "") return;
+    let queryStatement = {
+      name: searchValue.current,
+      minPrice: minPrice || 0,
+      maxPrice: maxPrice || 9000000,
+      page: page.current,
+      sortAsc: selectedSort.value,
+    };
+    if (selectedCategory.length) queryStatement.category = selectedCategory?.map((cat) => cat.value);
+    if (selectedGovernorate.length) queryStatement.location = selectedGovernorate?.map((gov) => gov.value);
+    fetch("http://127.0.0.1:3000/search?" + new URLSearchParams(queryStatement), {
+      headers: { cacheControl: "noCache" },
+    })
+      .then((res) => res.json())
+      .then((newProducts) => {
+        setProducts((oldProducts) => {
+          let totalProducts = oldProducts.concat(newProducts);
+          if (newProducts.length === 0) isLastPage.current = true;
+          return totalProducts;
+        });
+      });
+  }
+  function handleScrolling() {
+    let bottomReached = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight;
+    if (bottomReached && !isLastPage.current) {
+      page.current++;
+      fetchProducts();
     }
   }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrolling, {
+      passive: true,
+    });
+    return () => {
+      window.removeEventListener("scroll", handleScrolling);
+    };
+  }, []);
+
   return (
     <div className="app">
       <header className="app-header">
         <img className="app-logo" src={logo} alt="" />
         <div className="inputs-container">
           <div className="app-header-top">
-            <input id="search-input" placeholder="Search Price-Engine" name="search" onKeyUp={search} />
+            <input id="search-input" placeholder="Search Price-Engine" name="search" onKeyUp={handleSearchInput} />
             <Select
               className="combobox sort"
               options={sortOptions}
@@ -92,11 +123,13 @@ function App() {
       </header>
       <main>
         <div className="cards-container">
-          {products.map((product) => {
+          {products?.map((product) => {
             return (
               <div className="card" key={product.url}>
                 <img src={product.imgUrl} referrerPolicy="no-referrer" alt="" />
-                <h4 className="card-name" title={product.name}>{product.name}</h4>
+                <h4 className="card-name" title={product.name}>
+                  {product.name}
+                </h4>
                 <h3 className="card-price">{product.price} EGP</h3>
                 <a href={product.url} target="_blank" rel="noreferrer">
                   <button>Go to Page</button>
