f76143d74066e4d3a3893105710a5ade552f714a
Khalid Waleed <khalidwaleed0@outlook.com>
2024-12-16 00:31:54 +0200

    Added shop logo to each product card

--- src/App.js
+++ src/App.js
@@ -58,7 +58,14 @@ function App() {
       fetchProducts();
     }
   }
-
+  function importShopLogos(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      images[item.replace(/(\.\/)|(\.\w+$)/g, "")] = r(item);
+    });
+    return images;
+  }
+  const shopLogos = importShopLogos(require.context("./shopLogos", false, /\.(webp|png|jpe?g|svg)$/));
   useEffect(() => {
     window.addEventListener("scroll", handleScrolling, { passive: true });
     return () => window.removeEventListener("scroll", handleScrolling);
@@ -122,14 +129,19 @@ function App() {
           {products?.map((product) => {
             return (
               <div className="card" key={product.url}>
-                <img src={product.imgUrl} referrerPolicy="no-referrer" alt="" />
+                <img className="product-image" src={product.imgUrl} referrerPolicy="no-referrer" alt="" />
                 <h4 className="card-name" title={product.name}>
                   {product.name}
                 </h4>
                 <h3 className="card-price">{product.price} EGP</h3>
-                <a href={product.url} target="_blank" rel="noreferrer">
-                  <button>Go to Page</button>
-                </a>
+                <div className="links-container">
+                  <a href={product.shop.url} className="shop-logo-container">
+                    <img src={shopLogos[product.shop.name]} className="shop-logo" alt={product.shop.name} />
+                  </a>
+                  <a className="primary-btn" href={product.url} target="_blank" rel="noreferrer">
+                    <button>Go to Page</button>
+                  </a>
+                </div>
               </div>
             );
           })}
