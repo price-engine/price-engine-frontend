2811233ac829b328764c4b95b601e512be9b5e1a
Khalid Waleed <khalidwaleed0@outlook.com>
2025-03-09 17:50:04 +0200

    Redirect non-existing pages to HomePage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/share/:id" element={<ShareCartPage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
