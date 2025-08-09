87be09ec000b4cc3a1b9493990fa72705708b940
Khalid Waleed <khalidwaleed0@outlook.com>
2025-08-09 12:44:26 +0000

    Removed Github link

--- src/pages/HomePage/Footer.jsx
+++ src/pages/HomePage/Footer.jsx
@@ -1,5 +1,4 @@
 import linkedinIcon from "../../assets/icons/linkedin.svg";
-import githubIcon from "../../assets/icons/github.svg";
 import { Link } from "react-router";
 export default function Footer() {
   return (
@@ -7,9 +6,6 @@ export default function Footer() {
       <span className="socia-media-icons-container">
         <a href="https://www.linkedin.com/in/khalidwaleed0/" target="_blank" rel="nofollow noopener">
           <img src={linkedinIcon} className="linkedin-icon" alt="LinkedIn Profile" />
-        </a>
-        <a href="https://github.com/khalidwaleed0" target="_blank" rel="nofollow noopener">
-          <img src={githubIcon} className="github-icon" alt="Github Profile" />
         </a>
       </span>
       <p>Made with ❤️ © 2025 Price Engine.</p>
